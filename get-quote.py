f632dedfbf770f7ce9cf763a15fdee3c73a2b948
Morty Smith <iamtherickestmorty@gmail.com>
2021-06-30 19:36:07 +0100

    updates for issue 4

--- get-quote.py
+++ get-quote.py
@@ -8,8 +8,10 @@ def primary():
 
   last = len(quotes) - 1
   rnd = random.randint(0, last)
+  rnd2 = random.randint(0, last)
 
-  print(quotes[rnd])
+  print(quotes[rnd], end="")
+  print(quotes[rnd2], end="")
 
 if __name__== "__main__":
   primary()
